54c6dfb5c4df7cb10f1c0285ed25cb1b832a852d
Tabish Zaidi <szaidi@atb.com>
2024-06-06 12:09:33 -0600

    adding basic animation and sample name list to config.js

--- roulette.js
+++ roulette.js
@@ -23,14 +23,29 @@ function constructNames(chosenIndex, names, sillyNames) {
 	return indices.map((index) => index < 0 ? sillyName : names[index]);
 }
 
-let title = document.querySelector('h1');
+const MAX_FONT_SIZE = parseInt(window.innerWidth * 0.25); // in px
+console.log(`Max Font Size: ${MAX_FONT_SIZE}`);
+
+const title = document.getElementById('theName');
+
+function adjustFontSize(name) {
+  const textWidth = name.length; // Get actual width of the text
+  const maxWidth = window.innerWidth * 0.8;
+  // Calculate a new font size based on text width and desired max width
+  const newFontSize = Math.min(maxWidth / (textWidth/1.6), MAX_FONT_SIZE);
+  title.style.fontSize = parseInt(newFontSize) + 'px'; // Set the new font size
+	console.log(title.style.fontSize);
+}
 
 function shuffle(names) {
 	title.innerText = names[0];
-	let pause = Math.max(17, 1000 / names.length); // get gradually slower as the names run out
+	adjustFontSize(title.innerText);
+	let pause = Math.max(33, 1000 / names.length); // get gradually slower as the names run out
 	let newNames = names.splice(1,names.length);
 	if (newNames.length) {
 		window.setTimeout(() => {shuffle(newNames)}, pause);
+	} else {
+		title.className = 'animated-text';
 	}
 }
 
@@ -38,6 +53,7 @@ function shuffle(names) {
 let button = document.querySelector('#roulette');
 
 button.addEventListener('click', () => {
+	title.className = '';
 	let chosenIndex = Math.floor(Math.random() * config.names.length);
 	let nameList = constructNames(chosenIndex, config.names, config.sillyNames);
 	shuffle(nameList);
